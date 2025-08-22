fe00cec7ebd1b2b3bda30de37584de4ed340e22f
Neena <neenasebastian91@gmail.com>
2025-08-22 11:20:23 +0530

    First commit

---
+++ gitsample/src/gitsample/facebook.java
@@ -0,0 +1,15 @@
+package gitsample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class facebook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+
+}
